c81d80ffab1947f3bcc90fed9af18bca56e8cedb
Ameth Toledo <233363@ids.upchiapas.edu.mx>
2024-03-08 02:11:46 -0600

    Segunda Actualizacion de Proyecto-Dorikam

---
+++ IdeaProjects/Proyecto-Dorikam/src/main/java/com/toledo/proyectodorikam/controllers/AgregarProductoController.java
@@ -0,0 +1,34 @@
+package com.toledo.proyectodorikam.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class AgregarProductoController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button ExitButton;
+
+    @FXML
+    void OnMouseClickedExitButton(MouseEvent event) {
+        Stage stage = (Stage) ExitButton.getScene().getWindow();
+        stage.close();
+
+    }
+
+    @FXML
+    void initialize() {
+        assert ExitButton != null : "fx:id=\"ExitButton\" was not injected: check your FXML file 'agregar-productos-view.fxml'.";
+
+    }
+
+}
