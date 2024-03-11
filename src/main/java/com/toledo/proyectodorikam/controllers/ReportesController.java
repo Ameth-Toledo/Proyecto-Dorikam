ed8f61dc092a83aa46f739c0914dcc3fe980c9ce
Fabricio PRZ <233394@ids.upchiapas.edu.mx>
2024-03-11 02:05:18 -0600

    Interfaz de generar reportes

---
+++ src/main/java/com/toledo/proyectodorikam/controllers/ReportesController.java
@@ -0,0 +1,17 @@
+package com.toledo.proyectodorikam.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class ReportesController {
+
+    @FXML
+    private Button ExitButton;
+
+    @FXML
+    void OnMouseClickedExitButton(MouseEvent event) {
+
+    }
+
+}
