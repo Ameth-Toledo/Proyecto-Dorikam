a092a7a19f883a93f9001927d31d8e2b6e5ab878
Fabricio PRZ <233394@ids.upchiapas.edu.mx>
2024-03-31 17:49:16 -0600

    Interfaz de ver apartados

---
+++ src/main/java/com/toledo/proyectodorikam/controllers/VerApartadosController.java
@@ -0,0 +1,42 @@
+package com.toledo.proyectodorikam.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+
+public class VerApartadosController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ListView<?> ApartadosList;
+
+    @FXML
+    private Button ExitButton;
+
+    @FXML
+    private Button verApartados;
+
+    @FXML
+    void OnMouseClickedExitButton(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onMouseClickedVerReportes(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
