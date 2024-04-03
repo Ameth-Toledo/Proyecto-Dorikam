5f2d17f63f2ec0ef0a389f9ab3acb3244dde94d2
Ameth Toledo <233363@ids.upchiapas.edu.mx>
2024-04-02 22:32:50 -0600

    Implementacion localDate y LocalTime en el Home

--- src/main/java/com/toledo/proyectodorikam/controllers/HomeController.java
+++ src/main/java/com/toledo/proyectodorikam/controllers/HomeController.java
@@ -2,7 +2,13 @@ package com.toledo.proyectodorikam.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.toledo.proyectodorikam.App;
 import javafx.application.Platform;
@@ -10,6 +16,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
@@ -32,6 +39,13 @@ public class HomeController {
     @FXML
     private Button ExitButton;
 
+    @FXML
+    private Label clockLabel;
+
+    @FXML
+    private Label dateLabel;
+
+
     Stage callAdmin = new Stage();
 
     @FXML
@@ -53,6 +67,28 @@ public class HomeController {
 
     @FXML
     void initialize() {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(() -> {
+                    actualizarHora();
+                });
+            }
+        }, 0, 1000);
+        updateDateLabel();
+    }
+
+    private void actualizarHora() {
+        LocalTime horaActual = LocalTime.now();
+        String horaFormateada = String.format("%02d:%02d:%02d", horaActual.getHour(), horaActual.getMinute(), horaActual.getSecond());
+        clockLabel.setText(horaFormateada);
+    }
+
+    private void updateDateLabel() {
+        LocalDate date = LocalDate.now();
+        String formattedDate = date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        dateLabel.setText(formattedDate);
     }
 
     private void salirHome() {
